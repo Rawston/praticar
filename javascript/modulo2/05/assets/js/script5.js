3b976fc48f8641abe77f41ced08cd94d2224cc96
Rawston <houston_cam@msn.com>
2022-10-08 23:47:48 -0300

    Retorno da função!!

---
+++ javascript/modulo2/05/assets/js/script5.js
@@ -0,0 +1,9 @@
+/* Retorno da função */
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+let completo = nomeCompleto("Rawston", "Marinho");
+//ENTRADA -> PROCESSAMENTO -> SAÍDA
+
+//console.log(nomeCompleto("Rawston", "Marinho"));
