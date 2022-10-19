b205b828e496dd43ac31a039efceabaf395c71c3
Rawston <houston_cam@msn.com>
2022-10-19 01:26:55 -0300

    Concatenação!!

---
+++ Meus Cursos/javascript/Curso Javascript básico/assets/app.js
@@ -0,0 +1,24 @@
+/* Variaveis */
+var rawston = 'Rawston Marinho';
+console.log(rawston);
+//mudando de valor!!
+rawston = true;
+console.log(rawston);
+
+rawston = 3.14;
+console.log(rawston);
+
+rawston = 1975;
+console.log(rawston);
+
+// Concatenação!!
+//var name = 'Rawston';
+var name = prompt("O seu nome?");
+var age = prompt("Qual su idade?");
+
+//var age = 47;
+var ageTwo = 90;
+
+console.log('o ' + name + ' tem: ' + age + ' de idade.');
+console.log(age + ' ' + ageTwo);
+console.log(name + ' ' + age);
