aee225d4eaab58a89b7e3b753e0291d6144d1df9
Rawston <houston_cam@msn.com>
2022-10-18 22:46:09 -0300

    Curso de Vue.js

---
+++ javascript/vueEsp/js/app.js
@@ -0,0 +1,63 @@
+const myApp = {
+    data() {
+        return {
+            title: 'Curso de Vue 3',
+            name: 'Rawston',
+            lastName: 'Marinho',
+            products: [{
+                    title: 'Filme 01',
+                    description: 'Filme muito legal',
+                    image: './assets/images/filme01.jpg',
+                    stars: 3,
+                },
+                {
+                    title: 'Filme 02',
+                    description: 'Filme muito legal (2)',
+                    image: './assets/images/filme02.png',
+                    stars: 0,
+                }
+            ],
+            cart: [],
+            //textColor: '#000',
+            //bgColor: '#fff',
+            styles: {
+                backgroundColor: '#000',
+                color: '#fff',
+            },
+            themeBlack: true
+        }
+
+    },
+
+    computed: {
+        fullName() {
+            return this.name + ' ' + this.lastName;
+        }
+    },
+    methods: {
+        addCart(product) {
+            this.cart.push(product)
+        },
+        inCart(product) {
+            return this.cart.indexOf(product) != -1
+        },
+        removeCart(product) {
+            this.cart = this.cart.filter((prod, index) => product != prod)
+        },
+        toogleTmeme() {
+            this.themeBlack = !this.themeBlack
+            if (this.themeBlack) {
+                this.styles.backgroundColor = '#000'
+                this.styles.color = '#fff'
+            } else {
+                this.styles.backgroundColor = '#fff'
+                this.styles.color = '#000'
+            }
+
+        }
+
+    },
+
+}
+
+const app = Vue.createApp(myApp)
