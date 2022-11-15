2231a8ea1ec1222bb5e04391f3c2b8d051970043
Rawston <houston_cam@msn.com>
2022-11-15 19:25:04 -0300

    Lista de Tarefas!!

--- javascript/vue3-composition-api/src/views/TodosView/Todos-view.vue
+++ javascript/vue3-composition-api/src/views/TodosView/Todos-view.vue
@@ -1,3 +1,28 @@
 <template>
    <h1>Lista de Tarefas</h1>
+
+   <ul>
+      <li v-for="todo in todosView" :key="todo.identify">
+
+         {{ todo.name }}
+      </li>
+   </ul>
+
 </template>
+
+<script>
+export default {
+   name: "TodosView",
+   setup(){
+      const todosView = [
+{identify: 1, name: 'tarefa 01', completed: true},
+{identify: 2, name: 'tarefa 02', completed: true},
+{identify: 3, name: 'tarefa 03', completed: true},
+      ]
+
+      return {
+         todosView
+      }
+   }
+}
+</script>
