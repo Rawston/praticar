6501ea9d9298d0a0736f014fe379067fe3d53ccf
Rawston <houston_cam@msn.com>
2022-12-09 01:36:16 -0300

    erro Component name "Todos" should always be multi-word

--- javascript/vue3-composition-api/src/views/TodosView/Todos-view.vue
+++ javascript/vue3-composition-api/src/views/TodosView/Todos-view.vue
@@ -1,3 +1,32 @@
 <template>
    <h1>Lista de Tarefas</h1>
+
+   <ul>
+      <li v-for="todo in todos" :key="todo.identify">
+            {{ todo.name }}
+      </li>
+   
+   </ul>
 </template>
+
+
+
+<script>
+export default{
+   name: 'Todos',
+   setup(){
+      const todos = [
+         {identify: 1, name: 'tarefa 01', completed: true},
+         {identify: 2, name: 'tarefa 02', completed: false},
+         {identify: 3, name: 'tarefa 03', completed: true},
+      ]
+
+      return{
+         todos
+      }
+
+
+   }
+}
+
+</script>
